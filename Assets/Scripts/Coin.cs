6fe374c1f3807c026d2531b3fc978c6db1cd01b2
Malukker <163434018+Malukker@users.noreply.github.com>
2024-10-30 20:05:37 +0100

    Animations fixed, Coins Fixed

--- Assets/Scripts/Coin.cs
+++ Assets/Scripts/Coin.cs
@@ -1,9 +1,17 @@
+using System;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class Coin : Item
 {
+    [SerializeField] private int _value;
+    public static event Action<int> OnCoinPickup;
+    public static event Action<Coin> OnCoinDestruction; 
+
     protected override void Effect()
     {
-
+        OnCoinPickup?.Invoke(_value);
+        OnCoinDestruction?.Invoke(this);
+        Destroy(gameObject);
     }
 }
--- Assets/Scripts/CoinCounter.cs
+++ Assets/Scripts/CoinCounter.cs
@@ -1,16 +1,22 @@
+using NaughtyAttributes;
+using TMPro;
 using UnityEngine;
 
 public class CoinCounter : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    [ShowNonSerializedField] private int _totalAmount;
+    private TextMeshProUGUI _text;
+
     void Start()
     {
-        
+        Coin.OnCoinPickup += UpdateCounter;
+        _text = GetComponent<TextMeshProUGUI>();
+        UpdateCounter(0);
     }
 
-    // Update is called once per frame
-    void Update()
+    void UpdateCounter(int value)
     {
-        
+        _totalAmount += value;
+        _text.text = _totalAmount.ToString() + " coins";
     }
 }
